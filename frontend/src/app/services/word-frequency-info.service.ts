2bdcae782dbeaa103bfd73edbdda87c76a6f42e2
Swati707 <swatikanchan707@gmail.com>
2018-03-22 11:52:35 +0530

    Edited services

--- frontend/src/app/services/word-frequency-info.service.ts
+++ frontend/src/app/services/word-frequency-info.service.ts
@@ -5,12 +5,12 @@ import { Frequency } from '../model/frequency';
 @Injectable()
 export class WordFrequencyInfoService {
 
-  url: String = "http://localhost:3000/"
+  getUrl: String = "/"
 
   constructor(private http: HttpClient) { }
 
     //service to get top N most frequent words
     getTopNMostFrequentWords(number){
-      return this.http.get<Frequency[]>(this.url + number);
+      return this.http.get<Frequency[]>(this.getUrl + number);
     }
 }
